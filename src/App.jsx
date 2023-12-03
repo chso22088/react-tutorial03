49eae06ed2b6f5b7aa995ff199f83d589a846ed1
[chso22088] <[chso22088@g.nihon-u.ac.jp]>
2023-12-04 08:54:04 +0900

    2023/1204/0853

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,71 @@
+import React, { useState } from 'react';
+
+const images = [
+  {
+    src: 'images/pic1.jpg',
+    alt: 'Closeup of a human eye',
+  },
+  {
+    src: 'images/pic2.jpg',
+    alt: 'Rock that looks like a wave',
+  },
+  {
+    src: 'images/pic3.jpg',
+    alt: 'Purple and white pansies',
+  },
+  {
+    src: 'images/pic4.jpg',
+    alt: "Section of wall from a pharaoh's tomb",
+  },
+  {
+    src: 'images/pic5.jpg',
+    alt: 'Large moth on a leaf',
+  },
+];
+
 export default function App() {
-    return (
-      <>
-        <h1>Image gallery example</h1>
-        <div className="full-img">
-          <img
-            className="displayed-img"
-            src="images/pic1.jpg"
-            alt="Closeup of a human eye"
-          />
-          <div className="overlay"></div>
-          <button className="dark">Darken</button>
-        </div>
-        <div className="thumb-bar"></div>
-      </>
-    );
+  const [selectedImage, setSelectedImage] = useState(images[0]);
+  const [isDarkened, setIsDarkned] = useState(false);
+
+  function handleThumbnailClick (index) {
+    setSelectedImage(images[index]);
+    setIsDarkned(false);
+  };
+
+  function handleDarkenButtonClick() {
+    setIsDarkned(!isDarkened);
   }
+
+  const overlayStyle = {
+    backgroundColor: isDarkened ? 'rgba(0, 0, 0, 0.5)' : 'rgba(0, 0, 0, 0)'
+  };
+
+  const buttonLabel = isDarkened ? 'Lighten' : 'Darken';
+
+  return (
+    <>
+      <h1>Image gallery example</h1>
+      <div className="full-img">
+        <img
+          className="displayed-img"
+          src={selectedImage.src}
+          alt={selectedImage.alt}
+        />
+        <div className="overlay" style={overlayStyle}></div>
+        <button className="dark" onClick={handleDarkenButtonClick}>
+          {buttonLabel}
+        </button>
+      </div>
+      <div className="thumb-bar">
+        {images.map((image, index) => (
+          <img
+            key={index}
+            src={image.src}
+            alt={image.alt}
+            onClick={() => handleThumbnailClick(index)}
+          />
+        ))}
+      </div>
+    </>
+  );
+}
